f5efa5a76dc1bdd138ede57d93625bceab1a9953
hjbillings <66991780+hjbillings@users.noreply.github.com>
2020-10-22 19:55:17 -0400

    Update cut sites.py

--- cut sites.py
+++ cut sites.py
@@ -3,9 +3,9 @@
 sequence = "ACACACACACACACA"
 cut_site = "ACAC"
 cut_amt = 0
-#i = 0
   
 for i in range(0, len(sequence)):
     if sequence[i:(len(cut_site)+i)] == cut_site:
         cut_amt += 1
 print(cut_amt)
+
